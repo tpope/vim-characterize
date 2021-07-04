3e4e09833f88d9730c07fe44f86e30adff88bd0a
Tim Pope <code@tpope.net>
2021-07-04 10:09:37 -0400

    Add updating emoji to support script
    For reference: multi-character sequences (which we can't really do
    anything with) are encoded in the image URL separated by hyphens.
    Joining these together with U+200D ZERO WIDTH JOINER between each pair
    of characters is the correct way to reproduce the emoji, *except* when
    both characters are REGIONAL INDICATOR SYMBOL LETTER characters between
    U+1F1E6 and U+1F1FF, in which case they should be concatenated with no
    joiner in-between.

--- support/characterize.rb
+++ support/characterize.rb
@@ -1,13 +1,31 @@
 #!/usr/bin/env ruby
 
 require 'open-uri'
+require 'json'
 
 firsts = {}
 lasts = {}
-old = File.read('autoload/characterize.vim')[/.*?\nlet s:d = {}\n*/m]
+old = File.read('autoload/characterize.vim').sub(/let s:emojis = {.*/m, '')
 File.open('autoload/characterize.vim', 'w') do |out|
   out.write old
 
+  emojis = Hash.new { |h, k| h[k] = [] }
+  URI.open(ARGV[1] || 'https://api.github.com/emojis') do |f|
+    JSON.parse(f.read).each do |(name, img_url)|
+      code = img_url[%r{/emoji/unicode/([[:xdigit:]]{4,})\.png(?:\?|$)}, 1]
+      next unless code
+      emojis[code] << name
+    end
+  end
+  out.puts 'let s:emojis = {'
+  emojis.sort_by { |(k, v)| k.to_i(16) }.each do |(code, names)|
+    out.puts "      \\ 0x#{code}: [" + names.map { |n| "':#{n}:'" }.join(', ') + '],'
+  end
+  out.puts "      \\ }"
+  out.puts
+
+  out.puts "let s:d = {}"
+  out.puts
   URI.open(ARGV.first || 'https://unicode.org/Public/UNIDATA/UnicodeData.txt') do |f|
     f.each_line do |l|
       code, name, cat, ccc, bc, cdm, ddv, dv, nv, m, u1name, comment, ucase, lcase, tcase = *l.chomp.split(';', 15)
